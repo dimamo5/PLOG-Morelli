5e74cc66b3d7b3734fbb2e30bce4e2aaed27ca92
sergiomieic <sergio.mieic@gmail.com>
2015-10-11 01:44:46 +0100

    Melhoramentos visuais

--- Morelli/src/imprime.pl
+++ Morelli/src/imprime.pl
@@ -74,20 +74,38 @@ inverte(1,X):-  X is 0.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
 %%%%%%%%%%%%%%%%%% IMPRESSÃO DO TABULEIRO %%%%%%%%%%%%%%%%%%%%%%%
 
-imprime_tab(Tab):-     
-        imprime(13*2+2,['_']),nl,
-        imprime_linhas(Tab).
+line_graphic(['    ','   A  ', '   B  ','   C  ','   D  ','   E  ' ,'   F  ','   G  '
+             ,'   H  ','   I  ','   J  ','   K  ','   L  ','   M  ']).  
+
+imprime_tab(Tab):-
+        line_graphic(L),
+        imprime_aux(L), nl, 
+        write('   '),     
+        imprime(13*6+3,['-']), nl,
+        imprime_linhas(Tab,1).
+
+aux(N):-
+        N < 10,
+        write(N),
+        write('  |').
+aux(N):-
+        write(N),
+        write(' |').
         
  
-imprime_linhas([Head|Tail]):-   %descomentar em baixo para aspecto visual diferente
-        imprime_aux(Head), nl,  %write('|'), nl,imprime(13*2+2,['-']), nl,
-        imprime_linhas(Tail).
+imprime_linhas([Head|Tail],N):-   %descomentar em baixo para aspecto visual diferente
+        N1 is N+1,
+        aux(N),
+        imprime_aux(Head),  write(' |'),nl,
+         write('   '), imprime(13*6+3,['-']), nl,
+        imprime_linhas(Tail,N1).
+
  
-imprime_linhas([]).
+imprime_linhas([],_).
 
 /*funçao auxiliar que imprime no ecra*/
 imprime_aux([H|T]):-
-        mostra(H), %write(b),
+        mostra(H),
         imprime_aux(T).
 
 imprime_aux([]). /* caso base */
@@ -109,11 +127,11 @@ mostra(r):-
        mostra(X).
 
 mostra(b):-
-        write('| ').
+        write('   .  ').
 mostra(0):-
-        write('|X').
+        write('   X  ').
 mostra(1):-
-        write('|O').
+        write('   O  ').
 
 mostra(X):-
         write(X).
